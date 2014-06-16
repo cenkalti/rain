302eb6df75891b9b95011dc72027724b22ace2ac
Cenk Alti <cenkalti@gmail.com>
2014-06-16 16:40:55 +0300

    write piece to disk

--- rain/piece.go
+++ rain/piece.go
@@ -1,6 +1,7 @@
 package rain
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"errors"
 	"os"
@@ -49,6 +50,7 @@ func (p *piece) run() {
 
 			select {
 			case <-unchokeC:
+				pieceData := make([]byte, p.length)
 				for _, b := range p.blocks {
 					if err := peer.sendRequest(newPeerRequestMessage(p.index, b.index*blockSize, b.length)); err != nil {
 						p.log.Error(err)
@@ -57,19 +59,27 @@ func (p *piece) run() {
 					select {
 					case piece := <-p.pieceC:
 						p.log.Noticeln("received piece", len(piece.Block))
-
-						// TODO write block to disk
+						copy(pieceData[piece.Begin:], piece.Block)
 					case <-time.After(time.Minute):
 						p.log.Infof("Peer did not send piece #%d block #%d", p.index, b.index)
 					}
 				}
-				// TODO hash check
 
-				// TODO write downloaded piece
-				// _, err := p.write(nil)
-				// if err != nil {
-				// 	panic(err)
-				// }
+				// Verify piece hash
+				hash := sha1.New()
+				hash.Write(pieceData)
+				if bytes.Compare(hash.Sum(nil), p.sha1[:]) != 0 {
+					p.log.Error("received corrupt piece")
+					return
+				}
+
+				// Write downloaded piece
+				_, err := p.Write(pieceData)
+				if err != nil {
+					p.log.Error(err)
+					return
+				}
+				p.log.Debug("piece written successfully")
 			case <-time.After(time.Minute):
 				p.log.Info("Peer did not unchoke")
 			}
