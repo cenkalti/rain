d8b5e3c4ab4f3cd242793a3fc0bb88854aee70d3
Cenk Alti <cenkalti@gmail.com>
2014-07-13 12:56:49 +0300

    fmt

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -31,5 +31,10 @@ type logFormatter struct{}
 
 // Format outputs a message like "2014-02-28 18:15:57 [example] INFO     somethinfig happened"
 func (f logFormatter) Format(rec *log.Record) string {
-	return fmt.Sprintf("%s %-8s [%s] %-8s %s", fmt.Sprint(rec.Time)[:19], log.LevelNames[rec.Level], rec.LoggerName, filepath.Base(rec.Filename)+":"+strconv.Itoa(rec.Line), rec.Message)
+	return fmt.Sprintf("%s %-8s [%s] %-8s %s",
+		fmt.Sprint(rec.Time)[:19],
+		log.LevelNames[rec.Level],
+		rec.LoggerName,
+		filepath.Base(rec.Filename)+":"+strconv.Itoa(rec.Line),
+		rec.Message)
 }
