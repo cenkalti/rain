52a0e8610fe92afbbc3a9eb14ad9a5fc329d4125
Cenk Alti <cenkalti@gmail.com>
2014-09-18 23:12:48 +0300

    add connection encryption test

--- internal/connection/connection_test.go
+++ internal/connection/connection_test.go
@@ -4,6 +4,8 @@ import (
 	"net"
 	"testing"
 
+	"github.com/cenkalti/mse"
+
 	"github.com/cenkalti/rain/internal/connection"
 	"github.com/cenkalti/rain/internal/protocol"
 )
@@ -19,6 +21,7 @@ var (
 	id1      = protocol.PeerID{0x0C}
 	id2      = protocol.PeerID{0x0D}
 	infoHash = protocol.InfoHash{0x0E}
+	sKeyHash = mse.HashSKey(infoHash[:])
 )
 
 func TestUnencrypted(t *testing.T) {
@@ -26,6 +29,7 @@ func TestUnencrypted(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer l.Close()
 	done := make(chan struct{})
 	go func() {
 		conn, cipher, ext, id, err := connection.Dial(addr, false, false, ext1, infoHash, id1)
@@ -56,6 +60,64 @@ func TestUnencrypted(t *testing.T) {
 		t.Fatal(err)
 	}
 	if cipher != 0 {
+		t.Errorf("cipher: %d", cipher)
+	}
+	if ext != ext1 {
+		t.Errorf("ext: %s", ext)
+	}
+	if ih != infoHash {
+		t.Errorf("ih: %s", ih)
+	}
+	if id != id1 {
+		t.Errorf("id: %s", id)
+	}
+}
+
+func TestEncrypted(t *testing.T) {
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+	done := make(chan struct{})
+	go func() {
+		conn, cipher, ext, id, err := connection.Dial(addr, true, false, ext1, infoHash, id1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if conn == nil {
+			t.Errorf("conn: %s", conn)
+		}
+		if cipher != mse.RC4 {
+			t.Errorf("cipher: %d", cipher)
+		}
+		if ext != ext2 {
+			t.Errorf("ext: %s", ext)
+		}
+		if id != id2 {
+			t.Errorf("id: %s", id)
+		}
+		close(done)
+	}()
+	conn, err := l.Accept()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cipher, ext, ih, id, err := connection.Accept(
+		conn, false,
+		func(h [20]byte) (sKey []byte) {
+			if h == sKeyHash {
+				return infoHash[:]
+			}
+			return nil
+		},
+		func(ih protocol.InfoHash) bool { return ih == infoHash },
+		ext2, id2)
+	<-done
+	if err != nil {
+		t.Fatal(err)
+	}
+	if cipher != mse.RC4 {
 		t.Errorf("cipher: %d", cipher)
 	}
 	if ext != ext1 {
