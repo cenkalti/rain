a00c8ace31182e5e4c903cd903f415b73f9f12e2
Cenk Alti <cenkalti@gmail.com>
2018-09-11 14:21:39 +0300

    enable auto_register

--- xbt-docker/docker-run-xbt.sh
+++ xbt-docker/docker-run-xbt.sh
@@ -11,6 +11,8 @@ echo "MySQL is ready."
 cd /xbt/Tracker
 
 mysql -h xbtdocker_mysql_1 -u xbt -pxbt xbt < xbt_tracker.sql &>/dev/null
+mysql -h xbtdocker_mysql_1 -u xbt -pxbt xbt -e "delete from xbt_config where name='auto_register'" &>/dev/null
+mysql -h xbtdocker_mysql_1 -u xbt -pxbt xbt -e "insert into xbt_config values('auto_register', '1')" &>/dev/null
 
 ./xbt_tracker
 sleep infinity
