06b7db464395778d6d33306760b635942e7676f7
Cenk Alti <cenkalti@gmail.com>
2018-09-24 17:00:23 +0300

    add udp tracker test

--- torrent/torrent_test.go
+++ torrent/torrent_test.go
@@ -25,6 +25,7 @@ import (
 var (
 	trackerAddr    = ":5000"
 	torrentFile    = filepath.Join("testdata", "10mb.torrent")
+	torrentFileUDP = filepath.Join("testdata", "10mb_udp.torrent")
 	torrentDataDir = "testdata"
 	torrentName    = "10mb"
 	timeout        = 10 * time.Second
@@ -188,6 +189,64 @@ func TestDownloadMagnet(t *testing.T) {
 	f.Seek(0, io.SeekStart)
 	magnetLink := "magnet:?xt=urn:btih:0a8e2e8c9371a91e9047ed189ceffbc460803262&dn=10mb&tr=http%3A%2F%2F127.0.0.1%3A5000%2Fannounce"
 	t2, err := torrent.NewMagnet(magnetLink, 6882, sto2, res2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer t2.Close()
+
+	select {
+	case <-t2.NotifyComplete():
+	case err = <-t2.NotifyError():
+		t.Fatal(err)
+	case <-time.After(timeout):
+		panic("download did not finish")
+	}
+
+	cmd := exec.Command("diff", "-rq",
+		filepath.Join(torrentDataDir, torrentName),
+		filepath.Join(where, torrentName))
+	err = cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.RemoveAll(where)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDownloadTorrentUDP(t *testing.T) {
+	defer startHTTPTracker(t)()
+
+	where, err := ioutil.TempDir("", "rain-")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sto1 := filestorage.New(torrentDataDir)
+	res1 := newResumeFile(t)
+	defer res1.Close()
+
+	f, err := os.Open(torrentFileUDP)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t1, err := torrent.New(f, 6881, sto1, res1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer t1.Close()
+
+	// Wait for seeder to announce to tracker.
+	time.Sleep(100 * time.Millisecond)
+
+	sto2 := filestorage.New(where)
+	res2 := newResumeFile(t)
+	defer res2.Close()
+
+	f.Seek(0, io.SeekStart)
+	t2, err := torrent.New(f, 6882, sto2, res2)
 	if err != nil {
 		t.Fatal(err)
 	}
