55ef3457adb7ead7ea111f415b131c05b18d4f6c
Cenk Alti <cenkalti@gmail.com>
2018-10-10 11:53:54 +0300

    show all status

--- torrent/status.go
+++ torrent/status.go
@@ -8,14 +8,20 @@ type Status int
 
 const (
 	Stopped Status = iota
+	DownloadingMetadata
+	Allocating
+	Verifying
 	Downloading
 	Seeding
 )
 
 var statusStrings = map[Status]string{
 	0: "Stopped",
-	1: "Downloading",
-	2: "Seeding",
+	1: "Downloading Metadata",
+	2: "Allocating",
+	3: "Verifying",
+	4: "Downloading",
+	5: "Seeding",
 }
 
 func (m Status) String() string {
@@ -30,8 +36,17 @@ func (t *Torrent) status() Status {
 	if !t.running {
 		return Stopped
 	}
-	if !t.completed {
-		return Downloading
+	if t.allocator != nil {
+		return Allocating
 	}
-	return Seeding
+	if t.verifier != nil {
+		return Verifying
+	}
+	if t.completed {
+		return Seeding
+	}
+	if t.info == nil {
+		return DownloadingMetadata
+	}
+	return Downloading
 }
