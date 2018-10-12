c67b92e73fed1efaab1b7ea9c643e9bba78507fb
Cenk Alti <cenkalti@gmail.com>
2018-10-12 10:09:29 +0300

    remove lock in mse conn

--- torrent/internal/mse/conn.go
+++ torrent/internal/mse/conn.go
@@ -2,15 +2,12 @@ package mse
 
 import (
 	"net"
-	"sync"
 )
 
 // Conn is a wrapper around net.Conn that does encryption/decryption on Read/Write methods.
 type Conn struct {
 	net.Conn
 	*Stream
-	mr sync.Mutex
-	mw sync.Mutex
 }
 
 // WrapConn returns a new wrapper around conn. You must call HandshakeIncoming or
@@ -23,15 +20,11 @@ func WrapConn(conn net.Conn) *Conn {
 }
 
 func (c *Conn) Read(p []byte) (n int, err error) {
-	c.mr.Lock()
 	n, err = c.Stream.Read(p)
-	c.mr.Unlock()
 	return
 }
 
 func (c *Conn) Write(p []byte) (n int, err error) {
-	c.mw.Lock()
 	n, err = c.Stream.Write(p)
-	c.mw.Unlock()
 	return
 }
