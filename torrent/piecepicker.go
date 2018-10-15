12213afdb4790af5ffe031f929eb9a964c671106
Cenk Alti <cenkalti@gmail.com>
2018-10-15 09:13:00 +0300

    refactor

--- torrent/piecepicker.go
+++ torrent/piecepicker.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 
 	"github.com/cenkalti/rain/torrent/internal/infodownloader"
+	"github.com/cenkalti/rain/torrent/internal/peer"
 	"github.com/cenkalti/rain/torrent/internal/peerprotocol"
 	"github.com/cenkalti/rain/torrent/internal/piece"
 	"github.com/cenkalti/rain/torrent/internal/piecedownloader"
@@ -24,6 +25,14 @@ func (t *Torrent) nextInfoDownload() *infodownloader.InfoDownloader {
 }
 
 func (t *Torrent) nextPieceDownload() *piecedownloader.PieceDownloader {
+	pi, pe := t.findPieceAndPeer()
+	if pi == nil || pe == nil {
+		return nil
+	}
+	return piecedownloader.New(pi.Piece, pe, t.snubbedPieceDownloaderC, t.pieceDownloaderDoneC)
+}
+
+func (t *Torrent) findPieceAndPeer() (*piece.Piece, *peer.Peer) {
 	// TODO request first 4 pieces randomly
 	sort.Sort(piece.ByAvailability(t.sortedPieces))
 	for _, pi := range t.sortedPieces {
@@ -36,55 +45,48 @@ func (t *Torrent) nextPieceDownload() *piecedownloader.PieceDownloader {
 		if len(pi.HavingPeers) == 0 {
 			continue
 		}
-		// prefer allowed fast peers first
-		for pe := range pi.HavingPeers {
+		// prefer not snubbed peers first, then prefer allowed fast peers
+		pe := t.selectPeer(pi, true, true)
+		if pe != nil {
+			return pi, pe
+		}
+		pe = t.selectPeer(pi, true, false)
+		if pe != nil {
+			return pi, pe
+		}
+		pe = t.selectPeer(pi, false, true)
+		if pe != nil {
+			return pi, pe
+		}
+		pe = t.selectPeer(pi, false, false)
+		if pe != nil {
+			return pi, pe
+		}
+	}
+	return nil, nil
+}
+
+func (t *Torrent) selectPeer(pi *piece.Piece, skipSnubbed, allowedFastOnly bool) *peer.Peer {
+	for pe := range pi.HavingPeers {
+		if _, ok := t.pieceDownloaders[pe]; ok {
+			continue
+		}
+		if skipSnubbed {
 			if pe.Snubbed {
 				continue
 			}
+		}
+		if allowedFastOnly {
 			if _, ok := pi.AllowedFastPeers[pe]; !ok {
 				continue
 			}
-			if _, ok := t.pieceDownloaders[pe]; ok {
-				continue
-			}
-			// TODO selecting first peer having the piece, change to more smart decision
-			return piecedownloader.New(pi.Piece, pe, t.snubbedPieceDownloaderC, t.pieceDownloaderDoneC)
-		}
-		for pe := range pi.HavingPeers {
-			if pe.Snubbed {
-				continue
-			}
+		} else {
 			if pe.PeerChoking {
 				continue
 			}
-			if _, ok := t.pieceDownloaders[pe]; ok {
-				continue
-			}
-			// TODO selecting first peer having the piece, change to more smart decision
-			return piecedownloader.New(pi.Piece, pe, t.snubbedPieceDownloaderC, t.pieceDownloaderDoneC)
 		}
-		for pe := range pi.HavingPeers {
-			if _, ok := pi.AllowedFastPeers[pe]; !ok {
-				continue
-			}
-			if _, ok := t.pieceDownloaders[pe]; ok {
-				continue
-			}
-			pe.Snubbed = false
-			// TODO selecting first peer having the piece, change to more smart decision
-			return piecedownloader.New(pi.Piece, pe, t.snubbedPieceDownloaderC, t.pieceDownloaderDoneC)
-		}
-		for pe := range pi.HavingPeers {
-			if pe.PeerChoking {
-				continue
-			}
-			if _, ok := t.pieceDownloaders[pe]; ok {
-				continue
-			}
-			pe.Snubbed = false
-			// TODO selecting first peer having the piece, change to more smart decision
-			return piecedownloader.New(pi.Piece, pe, t.snubbedPieceDownloaderC, t.pieceDownloaderDoneC)
-		}
+		pe.Snubbed = false
+		return pe
 	}
 	return nil
 }
