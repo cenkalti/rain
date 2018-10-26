20a6ee39c2a1feadaf0f2f804c746e784630c4f9
Cenk Alti <cenkalti@gmail.com>
2018-10-26 15:50:34 +0300

    update stats when selected torrent is changed

--- internal/console/console.go
+++ internal/console/console.go
@@ -1,6 +1,7 @@
 package console
 
 import (
+	"errors"
 	"fmt"
 	"sort"
 	"sync"
@@ -13,18 +14,20 @@ import (
 )
 
 type Console struct {
-	client      *rainrpc.Client
-	torrents    []rainrpc.Torrent
-	errTorrents error
-	selectedID  uint64
-	stats       torrent.Stats
-	errStats    error
-	m           sync.Mutex
+	client       *rainrpc.Client
+	torrents     []rainrpc.Torrent
+	errTorrents  error
+	selectedID   uint64
+	stats        torrent.Stats
+	errStats     error
+	m            sync.Mutex
+	updateStatsC chan struct{}
 }
 
 func New(clt *rainrpc.Client) *Console {
 	return &Console{
-		client: clt,
+		client:       clt,
+		updateStatsC: make(chan struct{}),
 	}
 }
 
@@ -53,6 +56,19 @@ func (c *Console) Run() error {
 }
 
 func (c *Console) layout(g *gocui.Gui) error {
+	err := c.drawTorrents(g)
+	if err != nil {
+		return err
+	}
+	err = c.drawStats(g)
+	if err != nil {
+		return err
+	}
+	_, err = g.SetCurrentView("torrents")
+	return err
+}
+
+func (c *Console) drawTorrents(g *gocui.Gui) error {
 	c.m.Lock()
 	defer c.m.Unlock()
 
@@ -77,6 +93,15 @@ func (c *Console) layout(g *gocui.Gui) error {
 			}
 		}
 	}
+	return nil
+}
+
+func (c *Console) drawStats(g *gocui.Gui) error {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	maxX, maxY := g.Size()
+	halfY := maxY / 2
 	if v, err := g.SetView("stats", -1, halfY, maxX, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
@@ -99,8 +124,7 @@ func (c *Console) layout(g *gocui.Gui) error {
 			}
 		}
 	}
-	_, err := g.SetCurrentView("torrents")
-	return err
+	return nil
 }
 
 func (c *Console) updateLoop(g *gocui.Gui) {
@@ -110,11 +134,18 @@ func (c *Console) updateLoop(g *gocui.Gui) {
 		select {
 		case <-ticker.C:
 			c.doUpdate(g)
+		case <-c.updateStatsC:
+			c.doUpdateStats(g)
 		}
 	}
 }
 
 func (c *Console) doUpdate(g *gocui.Gui) {
+	c.doUpdateTorrents(g)
+	c.doUpdateStats(g)
+}
+
+func (c *Console) doUpdateTorrents(g *gocui.Gui) {
 	resp, err := c.client.ListTorrents()
 
 	sort.Slice(resp.Torrents, func(i, j int) bool { return resp.Torrents[i].ID < resp.Torrents[j].ID })
@@ -123,22 +154,33 @@ func (c *Console) doUpdate(g *gocui.Gui) {
 	c.torrents = resp.Torrents
 	c.errTorrents = err
 	if len(c.torrents) == 0 {
-		c.selectedID = 0
+		c.setSelectedID(0)
 	} else if c.selectedID == 0 {
-		c.selectedID = c.torrents[0].ID
+		c.setSelectedID(c.torrents[0].ID)
 	}
+	c.m.Unlock()
+
+	g.Update(c.drawTorrents)
+}
+
+func (c *Console) doUpdateStats(g *gocui.Gui) {
+	c.m.Lock()
 	selectedID := c.selectedID
 	c.m.Unlock()
 
-	g.Update(c.layout)
+	if selectedID != 0 {
+		resp, err := c.client.GetTorrentStats(selectedID)
+		c.m.Lock()
+		c.stats = resp.Stats
+		c.errStats = err
+		c.m.Unlock()
+	} else {
+		c.m.Lock()
+		c.errStats = errors.New("no torrent selected")
+		c.m.Unlock()
+	}
 
-	resp2, err := c.client.GetTorrentStats(selectedID)
-	c.m.Lock()
-	c.stats = resp2.Stats
-	c.errStats = err
-	c.m.Unlock()
-
-	g.Update(c.layout)
+	g.Update(c.drawStats)
 }
 
 func quit(g *gocui.Gui, v *gocui.View) error {
@@ -159,7 +201,7 @@ func (c *Console) cursorDown(g *gocui.Gui, v *gocui.View) error {
 			return err
 		}
 	}
-	c.selectedID = c.torrents[cy+oy+1].ID
+	c.setSelectedID(c.torrents[cy+oy+1].ID)
 	return nil
 }
 
@@ -177,7 +219,7 @@ func (c *Console) cursorUp(g *gocui.Gui, v *gocui.View) error {
 			return err
 		}
 	}
-	c.selectedID = c.torrents[cy+oy-1].ID
+	c.setSelectedID(c.torrents[cy+oy-1].ID)
 	return nil
 }
 
@@ -188,4 +230,15 @@ func (c *Console) removeTorrent(g *gocui.Gui, v *gocui.View) error {
 
 	_, err := c.client.RemoveTorrent(id)
 	return err
+}
+
+func (c *Console) setSelectedID(id uint64) {
+	changed := id != c.selectedID
+	c.selectedID = id
+	if changed {
+		select {
+		case c.updateStatsC <- struct{}{}:
+		default:
+		}
+	}
 }
