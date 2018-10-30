06a27ae4375233010d0d4e0171eaf586ea524704
Cenk Alti <cenkalti@gmail.com>
2018-10-30 16:37:03 +0300

    send correct bytesleft to tracker

--- torrent/announce.go
+++ torrent/announce.go
@@ -11,12 +11,13 @@ func (t *Torrent) announcerFields() tracker.Transfer {
 		InfoHash: t.infoHash,
 		PeerID:   t.peerID,
 		Port:     t.port,
+		// TODO send BytesDownloaded to tracker
+		// TODO send BytesUploaded to tracker
 	}
 	if t.bitfield == nil {
 		tr.BytesLeft = math.MaxUint32
 	} else {
-		// TODO this is wrong, pre-calculate complete and incomplete bytes
-		tr.BytesLeft = t.info.TotalLength - int64(t.info.PieceLength)*int64(t.bitfield.Count())
+		tr.BytesLeft = t.info.TotalLength - t.bytesComplete()
 	}
 	return tr
 }
