747cdd9b121045665e0ea9e730493579e44b3d9e
Cenk Alti <cenkalti@gmail.com>
2019-01-17 15:55:07 +0300

    return same transport for all http trackers

--- client/trackermanager/trackermanager.go
+++ client/trackermanager/trackermanager.go
@@ -13,23 +13,16 @@ import (
 )
 
 type TrackerManager struct {
-	httpHostCount  map[string]int
-	udpHostCount   map[string]int
-	httpTransports map[string]*http.Transport
-	udpTransports  map[string]*udptracker.Transport
-	httpHosts      map[*httptracker.HTTPTracker]string
-	udpHosts       map[*udptracker.UDPTracker]string
-	m              sync.Mutex
+	httpTransport *http.Transport
+	udpTransports map[string]*udptracker.Transport
+	m             sync.Mutex
 }
 
 func New() *TrackerManager {
+	httpTransport := new(http.Transport)
 	return &TrackerManager{
-		httpHostCount:  make(map[string]int),
-		udpHostCount:   make(map[string]int),
-		httpTransports: make(map[string]*http.Transport),
-		udpTransports:  make(map[string]*udptracker.Transport),
-		httpHosts:      make(map[*httptracker.HTTPTracker]string),
-		udpHosts:       make(map[*udptracker.UDPTracker]string),
+		httpTransport: httpTransport,
+		udpTransports: make(map[string]*udptracker.Transport),
 	}
 }
 
@@ -42,14 +35,7 @@ func (m *TrackerManager) Get(s string, httpTimeout time.Duration, httpUserAgent 
 	defer m.m.Unlock()
 	switch u.Scheme {
 	case "http", "https":
-		t := m.httpTransports[u.Host]
-		if t == nil {
-			t = new(http.Transport)
-			m.httpTransports[u.Host] = t
-		}
-		m.httpHostCount[u.Host]++
-		tr := httptracker.New(s, u, httpTimeout, t, httpUserAgent)
-		m.httpHosts[tr] = u.Host
+		tr := httptracker.New(s, u, httpTimeout, m.httpTransport, httpUserAgent)
 		return tr, nil
 	case "udp":
 		t := m.udpTransports[u.Host]
@@ -57,40 +43,9 @@ func (m *TrackerManager) Get(s string, httpTimeout time.Duration, httpUserAgent 
 			t = udptracker.NewTransport(u.Host)
 			m.udpTransports[u.Host] = t
 		}
-		m.udpHostCount[u.Host]++
 		tr := udptracker.New(s, u.RequestURI(), t)
-		m.udpHosts[tr] = u.Host
 		return tr, nil
 	default:
 		return nil, fmt.Errorf("unsupported tracker scheme: %s", u.Scheme)
-	}
-}
-
-func (m *TrackerManager) Release(trk tracker.Tracker) {
-	m.m.Lock()
-	defer m.m.Unlock()
-	switch t := trk.(type) {
-	case *httptracker.HTTPTracker:
-		host := m.httpHosts[t]
-		delete(m.httpHosts, t)
-		m.httpHostCount[host]--
-		if m.httpHostCount[host] == 0 {
-			tr := m.httpTransports[host]
-			tr.CloseIdleConnections()
-			delete(m.httpHostCount, host)
-			delete(m.httpTransports, host)
-		}
-	case *udptracker.UDPTracker:
-		host := m.udpHosts[t]
-		delete(m.udpHosts, t)
-		m.udpHostCount[host]--
-		if m.udpHostCount[host] == 0 {
-			tr := m.udpTransports[host]
-			tr.Close()
-			delete(m.udpHostCount, host)
-			delete(m.udpTransports, host)
-		}
-	default:
-		panic("unknown tracker type")
 	}
 }
