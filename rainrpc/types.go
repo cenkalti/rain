7831dda46e8a126b59a91b8254d4ecd558515a93
Cenk Alti <cenkalti@gmail.com>
2019-01-24 17:57:43 +0300

    add json tags from rpc types

--- rainrpc/types.go
+++ rainrpc/types.go
@@ -2,131 +2,131 @@
 package rainrpc
 
 type Torrent struct {
-	ID       uint64
-	Name     string
-	InfoHash string
-	Port     uint16
+	ID       uint64 `json:"id"`
+	Name     string `json:"name"`
+	InfoHash string `json:"info_hash"`
+	Port     uint16 `json:"port"`
 }
 
 type Peer struct {
-	Addr string
+	Addr string `json:"addr"`
 }
 
 type Tracker struct {
-	URL      string
-	Status   string
-	Leechers int
-	Seeders  int
-	Error    *string
+	URL      string  `json:"url"`
+	Status   string  `json:"status"`
+	Leechers int     `json:"leechers"`
+	Seeders  int     `json:"seeders"`
+	Error    *string `json:"error"`
 }
 
 type Stats struct {
-	Status string
-	Error  *string
+	Status string  `json:"status"`
+	Error  *string `json:"error"`
 	Pieces struct {
-		Have      uint32
-		Missing   uint32
-		Available uint32
-		Total     uint32
-	}
+		Have      uint32 `json:"have"`
+		Missing   uint32 `json:"missing"`
+		Available uint32 `json:"available"`
+		Total     uint32 `json:"total"`
+	} `json:"pieces"`
 	Bytes struct {
-		Complete   int64
-		Incomplete int64
-		Total      int64
-		Downloaded int64
-		Uploaded   int64
-		Wasted     int64
-	}
+		Complete   int64 `json:"complete"`
+		Incomplete int64 `json:"incomplete"`
+		Total      int64 `json:"total"`
+		Downloaded int64 `json:"downloaded"`
+		Uploaded   int64 `json:"uploaded"`
+		Wasted     int64 `json:"wasted"`
+	} `json:"bytes"`
 	Peers struct {
-		Total    int
-		Incoming int
-		Outgoing int
-	}
+		Total    int `json:"total"`
+		Incoming int `json:"incoming"`
+		Outgoing int `json:"outgoing"`
+	} `json:"peers"`
 	Handshakes struct {
-		Total    int
-		Incoming int
-		Outgoing int
-	}
-	ReadyAddresses int
+		Total    int `json:"total"`
+		Incoming int `json:"incoming"`
+		Outgoing int `json:"outgoing"`
+	} `json:"handshakes"`
+	ReadyAddresses int `json:"ready_addresses"`
 	Downloads      struct {
-		Total   int
-		Running int
-		Snubbed int
-		Choked  int
-	}
+		Total   int `json:"total"`
+		Running int `json:"running"`
+		Snubbed int `json:"snubbed"`
+		Choked  int `json:"choked"`
+	} `json:"downloads"`
 	MetadataDownloads struct {
-		Total   int
-		Snubbed int
-		Running int
-	}
-	Name        string
-	Private     bool
-	PieceLength uint32
+		Total   int `json:"total"`
+		Snubbed int `json:"snubbed"`
+		Running int `json:"running"`
+	} `json:"metadata_downloads"`
+	Name        string `json:"name"`
+	Private     bool   `json:"private"`
+	PieceLength uint32 `json:"piece_length"`
 }
 
 type ListTorrentsRequest struct {
 }
 
 type ListTorrentsResponse struct {
-	Torrents []Torrent
+	Torrents []Torrent `json:"torrents"`
 }
 
 type AddTorrentRequest struct {
-	Torrent string
+	Torrent string `json:"torrent"`
 }
 
 type AddTorrentResponse struct {
-	Torrent Torrent
+	Torrent Torrent `json:"torrent"`
 }
 
 type AddURIRequest struct {
-	URI string
+	URI string `json:"uri"`
 }
 
 type AddURIResponse struct {
-	Torrent Torrent
+	Torrent Torrent `json:"torrent"`
 }
 
 type RemoveTorrentRequest struct {
-	ID uint64
+	ID uint64 `json:"id"`
 }
 
 type RemoveTorrentResponse struct {
 }
 
 type GetTorrentStatsRequest struct {
-	ID uint64
+	ID uint64 `json:"id"`
 }
 
 type GetTorrentStatsResponse struct {
-	Stats Stats
+	Stats Stats `json:"stats"`
 }
 
 type GetTorrentTrackersRequest struct {
-	ID uint64
+	ID uint64 `json:"id"`
 }
 
 type GetTorrentTrackersResponse struct {
-	Trackers []Tracker
+	Trackers []Tracker `json:"trackers"`
 }
 
 type GetTorrentPeersRequest struct {
-	ID uint64
+	ID uint64 `json:"id"`
 }
 
 type GetTorrentPeersResponse struct {
-	Peers []Peer
+	Peers []Peer `json:"peers"`
 }
 
 type StartTorrentRequest struct {
-	ID uint64
+	ID uint64 `json:"id"`
 }
 
 type StartTorrentResponse struct {
 }
 
 type StopTorrentRequest struct {
-	ID uint64
+	ID uint64 `json:"id"`
 }
 
 type StopTorrentResponse struct {
