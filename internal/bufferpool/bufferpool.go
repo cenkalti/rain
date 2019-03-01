a98fc69d75a85e8be31b85b87f265931e18dac3a
Cenk Alti <cenkalti@gmail.com>
2019-03-01 15:19:32 +0300

    fix allocaltion

--- internal/bufferpool/bufferpool.go
+++ internal/bufferpool/bufferpool.go
@@ -10,31 +10,33 @@ func New(buflen int) *Pool {
 	return &Pool{
 		pool: sync.Pool{
 			New: func() interface{} {
-				return make([]byte, buflen)
+				b := make([]byte, buflen)
+				return &b
 			},
 		},
 	}
 }
 
 func (p *Pool) Get(datalen int) Buffer {
-	buf := p.pool.Get().([]byte)
+	buf := p.pool.Get().(*[]byte)
 	return newBuffer(buf, datalen, p)
 }
 
 type Buffer struct {
 	Data []byte
-	buf  []byte
+	buf  *[]byte
 	pool *Pool
 }
 
-func newBuffer(buf []byte, length int, pool *Pool) Buffer {
+func newBuffer(buf *[]byte, length int, pool *Pool) Buffer {
 	return Buffer{
-		Data: buf[:length],
+		Data: (*buf)[:length],
 		buf:  buf,
 		pool: pool,
 	}
 }
 
 func (b Buffer) Release() {
+	// argument to Put should be pointer-like to avoid allocations
 	b.pool.pool.Put(b.buf)
 }
