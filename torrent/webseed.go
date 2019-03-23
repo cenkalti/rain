45e9df5af51786163c2549234f9588640ba980a1
Cenk Alti <cenkalti@gmail.com>
2019-03-23 09:01:38 +0300

    handle webseed piece result

--- torrent/webseed.go
+++ torrent/webseed.go
@@ -1,11 +1,36 @@
 package torrent
 
 import (
-	"github.com/cenkalti/rain/internal/webseeddownloader"
+	"github.com/cenkalti/rain/internal/piecewriter"
+	"github.com/cenkalti/rain/internal/urldownloader"
 )
 
-func (t *torrent) setWebseedDownloader() {
-	if len(t.webseedSources) > 0 {
-		t.webseedDownloader = webseeddownloader.New(t.webseedClient, t.webseedSources, t.bitfield)
+func (t *torrent) handleWebseedPieceResult(msg *urldownloader.PieceResult) {
+	if msg.Error != nil {
+		t.log.Errorln("webseed download error:", msg.Error)
+		// TODO handle error
+		return
 	}
+
+	t.log.Debugln("piece download completed. index:", msg.Index)
+
+	piece := &t.pieces[msg.Index]
+
+	t.resumerStats.BytesDownloaded += int64(len(msg.Buffer.Data))
+
+	// TODO update download speed
+	// t.downloadSpeed.Update(int64(len(msg.Buffer.Data)))
+
+	if piece.Writing {
+		panic("piece is already writing")
+	}
+	piece.Writing = true
+
+	// TODO suspend webseed piece result chan
+	// TODO suspend webseed piece result chan on peer message
+	// Prevent receiving piece messages to avoid more than 1 write per torrent.
+	t.pieceMessagesC.Suspend()
+
+	pw := piecewriter.New(piece, msg.Downloader, msg.Buffer)
+	go pw.Run(t.pieceWriterResultC, t.doneC)
 }
