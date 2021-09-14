2d9d1d84dcdb79d94e3de2b53157d18a88da3e2b
Benjamin Sergeant <bsergean@gmail.com>
2021-09-14 09:44:28 -0700

    Fix data race in Tier.go Announce method
    Fix #25 by adding a mutex. There is probably a more lightweight approach (atomic) but that does fix the data race in my unittest.

--- internal/tracker/tier.go
+++ internal/tracker/tier.go
@@ -3,6 +3,7 @@ package tracker
 import (
 	"context"
 	"math/rand"
+	"sync"
 )
 
 // Tier implements the Tracker interface and contains multiple Trackers which tries to announce to the working Tracker.
@@ -18,12 +19,16 @@ func NewTier(trackers []Tracker) *Tier {
 	rand.Shuffle(len(trackers), func(i, j int) { trackers[i], trackers[j] = trackers[j], trackers[i] })
 	return &Tier{
 		Trackers: trackers,
+		mutex: &sync.Mutex{},
 	}
 }
 
 // Announce a torrent to the tracker.
 // If annouce fails, the next announce will be made to the next Tracker in the tier.
 func (t *Tier) Announce(ctx context.Context, req AnnounceRequest) (*AnnounceResponse, error) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	
 	resp, err := t.Trackers[t.index].Announce(ctx, req)
 	if err != nil {
 		t.index = (t.index + 1) % len(t.Trackers)
