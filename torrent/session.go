d29ae5cd94b85772f8c00e11d3388307065bf264
Benjamin Sergeant <benjamin.sergeant@applovin.com>
2021-09-29 10:36:57 -0700

    avoid data races with session torrentsByInfoHash access

--- torrent/session.go
+++ torrent/session.go
@@ -60,10 +60,11 @@ type Session struct {
 	mPeerRequests   sync.Mutex
 	dhtPeerRequests map[*torrent]struct{}
 
-	mTorrents          sync.RWMutex
-	torrents           map[string]*Torrent
-	torrentsByInfoHash map[dht.InfoHash][]*Torrent
-	invalidTorrentIDs  []string
+	mTorrents           sync.RWMutex
+	torrents            map[string]*Torrent
+	mTorrentsByInfoHash sync.RWMutex
+	torrentsByInfoHash  map[dht.InfoHash][]*Torrent
+	invalidTorrentIDs   []string
 
 	mPorts         sync.RWMutex
 	availablePorts map[int]struct{}
@@ -348,6 +349,8 @@ func (s *Session) removeTorrentFromClient(id string) (*Torrent, error) {
 
 	// Delete from the list of torrents with same info hash
 	ih := dht.InfoHash(t.torrent.InfoHash())
+	s.mTorrentsByInfoHash.Lock()
+	defer s.mTorrentsByInfoHash.Unlock()
 	a := s.torrentsByInfoHash[ih]
 	for i, it := range a {
 		if it == t {
