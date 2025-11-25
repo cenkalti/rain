a8dba436ac8263f17e67bce09851070ead0d39f9
Cenk Altı <cenkalti@gmail.com>
2025-11-25 02:29:21 -0500

    use stdlib semaphore

--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/zeebo/bencode v1.0.0
 	go.etcd.io/bbolt v1.4.3
 	go.uber.org/goleak v1.3.0
+	golang.org/x/sync v0.10.0
 	golang.org/x/sys v0.38.0
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -63,7 +64,6 @@ require (
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/youtube/vitess v3.0.0-rc.3+incompatible // indirect
 	golang.org/x/crypto v0.45.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.1.6 // indirect
--- internal/semaphore/semaphore.go
+++ internal/semaphore/semaphore.go
@@ -1,20 +1,25 @@
 package semaphore
 
-import "sync/atomic"
+import (
+	"context"
+	"sync/atomic"
+
+	"golang.org/x/sync/semaphore"
+)
 
 // Semaphore used to control access to a common resource by multiple goroutines.
+// This is a wrapper around golang.org/x/sync/semaphore.Weighted with additional
+// metrics tracking for waiting and active counts.
 type Semaphore struct {
-	c       chan token
+	sem     *semaphore.Weighted
 	waiting int32
 	active  int32
 }
 
-type token struct{}
-
 // New returns a new counting semaphore of length `n`.
 func New(n int) *Semaphore {
 	return &Semaphore{
-		c: make(chan token, n),
+		sem: semaphore.NewWeighted(int64(n)),
 	}
 }
 
@@ -29,15 +34,20 @@ func (s *Semaphore) Len() int {
 }
 
 // Wait for the semaphore. Blocks until the resource is available.
-func (s *Semaphore) Wait() {
+// Returns an error if the context is cancelled before acquiring the semaphore.
+func (s *Semaphore) Wait() error {
 	atomic.AddInt32(&s.waiting, 1)
-	s.c <- token{}
+	err := s.sem.Acquire(context.TODO(), 1)
 	atomic.AddInt32(&s.waiting, -1)
+	if err != nil {
+		return err
+	}
 	atomic.AddInt32(&s.active, 1)
+	return nil
 }
 
 // Signal the semaphore. A random waiting goroutine will be waken up.
 func (s *Semaphore) Signal() {
-	<-s.c
+	s.sem.Release(1)
 	atomic.AddInt32(&s.active, -1)
 }
